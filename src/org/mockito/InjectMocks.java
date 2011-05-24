734591b8aa127a58b0202943716d7ec43395e310
Brice Dutheil <brice.dutheil@gmail.com>
2011-05-24 19:00:49 +0200

    issue 264 : Documentation fix

--- src/org/mockito/InjectMocks.java
+++ src/org/mockito/InjectMocks.java
@@ -50,9 +50,9 @@ import static java.lang.annotation.ElementType.FIELD;
  * <p>
  * <b><code>MockitoAnnotations.injectMocks(this)</code></b> method has to called to initialize annotated objects.
  * <p>
- * In above example, <code>injectMocks()</code> is called in &#064;Before (JUnit4) method of test's base class.
- * For JUnit3 <code>injectMocks()</code> can go to <code>setup()</code> method of a base class.
- * You can also put injectMocks() in your JUnit runner (&#064;RunWith) or use built-in runners: {@link org.mockito.runners.MockitoJUnitRunner}
+ * In above example, <code>initMocks()</code> is called in &#064;Before (JUnit4) method of test's base class.
+ * For JUnit3 <code>initMocks()</code> can go to <code>setup()</code> method of a base class.
+ * You can also put initMocks() in your JUnit runner (&#064;RunWith) or use built-in runners: {@link org.mockito.runners.MockitoJUnitRunner}
  */
 @Documented
 @Target( { FIELD })
