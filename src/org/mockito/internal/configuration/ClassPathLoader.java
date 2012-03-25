782d28a4c49b0aa1b9251c1df3ed6405c3a4b9d5
David Wallace <David Wallace@localhost>
2012-03-25 18:56:02 +1300

    Added MockingDetails; also tweaked Mockito javadocs

---
+++ src/org/mockito/internal/configuration/ClassPathLoader.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockito.internal.configuration;
+
+import java.util.ServiceLoader;
+import org.mockito.configuration.IMockitoConfiguration;
+import org.mockito.exceptions.misusing.MockitoConfigurationException;
+import org.mockito.plugins.MockMaker;
+import org.mockito.internal.creation.CglibMockMaker;
+
+public class ClassPathLoader {
+    
+    /**
+     * @return configuration loaded from classpath or null
+     */
+    @SuppressWarnings({"unchecked"})
+    public IMockitoConfiguration loadConfiguration() {
+        //Trying to get config from classpath
+        Class configClass = null;
+        try {
+            configClass = (Class) Class.forName("org.mockito.configuration.MockitoConfiguration");
+        } catch (ClassNotFoundException e) {
+            //that's ok, it means there is no global config, using default one. 
+            return null;
+        }
+        
+        try {
+            return (IMockitoConfiguration) configClass.newInstance();
+        } catch (ClassCastException e) {
+            throw new MockitoConfigurationException("\n" +
+                    "MockitoConfiguration class must implement org.mockito.configuration.IMockitoConfiguration interface.", e);
+        } catch (Exception e) {
+            throw new MockitoConfigurationException("\n" +
+                    "Unable to instantiate org.mockito.configuration.MockitoConfiguration class. Does it have a safe, no-arg constructor?", e);
+        }
+    }
+
+    /**
+     * Returns the best mock maker available for the current runtime. This scans
+     * the classpath to find a mock maker plugin if one is available, allowing
+     * mockito to run on alternative platforms like Android.
+     */
+    public static MockMaker getMockMaker() {
+        for (MockMaker mockMaker : ServiceLoader.load(MockMaker.class)) {
+            return mockMaker; // return the first one service loader finds (if any)
+        }
+        return new CglibMockMaker(); // default implementation
+    }
+}
---
+++ src/org/mockito/internal/creation/CglibMockMaker.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2012 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockito.internal.creation;
+
+import org.mockito.cglib.proxy.Callback;
+import org.mockito.cglib.proxy.Factory;
+import org.mockito.plugins.MockMaker;
+import org.mockito.plugins.MockitoInvocationHandler;
+import org.mockito.internal.creation.jmock.ClassImposterizer;
+import org.mockito.plugins.MockSettingsInfo;
+
+/**
+ * A MockMaker that uses cglib to generate mocks on a JVM.
+ */
+public final class CglibMockMaker implements MockMaker {
+
+    public <T> T createMock(Class<T> typeToMock, Class<?>[] extraInterfaces,
+            MockitoInvocationHandler handler, MockSettingsInfo settings) {
+        return ClassImposterizer.INSTANCE.imposterise(
+                new MethodInterceptorFilter(handler, settings), typeToMock, extraInterfaces);
+    }
+    
+    public void resetMock(Object mock, MockitoInvocationHandler newHandler, MockSettingsInfo settings) {
+        ((Factory) mock).setCallback(0, new MethodInterceptorFilter(newHandler, settings));
+    }
+
+    public MockitoInvocationHandler getHandler(Object mock) {
+        if (!(mock instanceof Factory)) {
+            return null;
+        }
+        Factory factory = (Factory) mock;
+        Callback callback = factory.getCallback(0);
+        if (!(callback instanceof MethodInterceptorFilter)) {
+            return null;
+        }
+        return ((MethodInterceptorFilter) callback).getHandler();
+    }
+}
